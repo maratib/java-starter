2e9314b65c46829e69fee980a38a53912399d07f
Maratib Ali Khan <maratib@gmail.com>
2025-01-11 21:05:21 +0500

    Updated

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ with
 - Java 11
 - JUnit 5
 - VSCode test settings
+
+```bash
+# To create jar file run
+mvn package
+# or
+mvn clean package
+```
--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,8 +1,10 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class Tests {
 
     @Test
+    @DisplayName("1 + 1 = 2")
     public void test1() {
         System.out.println("Hello from Test1");
     }
